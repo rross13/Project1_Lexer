8d056c4c208e5047c79098691a59e2c3ac81167c
rross13 <rross13>
2021-12-06 13:09:02 -0700

    changed input file

---
+++ cmake-build-debug-local/CMakeFiles/Project1.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/AddAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/AddAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/ColonAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/ColonAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/ColonDashAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/ColonDashAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/CommaAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/CommaAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/CommentAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/CommentAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/FactsAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/FactsAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/IDAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/IDAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/LeftParenAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/LeftParenAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/Lexer.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/Lexer.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/LineCommentAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/LineCommentAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/MultiplyAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/MultiplyAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/Parser.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/Parser.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/PeriodAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/PeriodAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/QMarkAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/QMarkAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/QueriesAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/QueriesAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/RightParenAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/RightParenAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/RulesAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/RulesAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/SchemesAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/SchemesAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/StringAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/StringAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/Token.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/Token.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/WhiteSpaceAutomaton.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/WhiteSpaceAutomaton.cpp.o"
+  "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/main.cpp" "/Users/rileyross/CLionProjects/CS236/Project1-Lexer/Project1/cmake-build-debug-local/CMakeFiles/Project1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-remote/CMakeFiles/Project1.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project1.dir/AddAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/ColonAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/ColonDashAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/CommaAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/CommentAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/FactsAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/IDAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/LeftParenAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/Lexer.cpp.o"
+  "CMakeFiles/Project1.dir/LineCommentAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/MultiplyAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/Parser.cpp.o"
+  "CMakeFiles/Project1.dir/PeriodAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/QMarkAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/QueriesAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/RightParenAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/RulesAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/SchemesAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/StringAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/Token.cpp.o"
+  "CMakeFiles/Project1.dir/WhiteSpaceAutomaton.cpp.o"
+  "CMakeFiles/Project1.dir/main.cpp.o"
+  "Project1"
+  "Project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
